de8cde8b27919897ec5df5eef97d358a3ac1dd5c
govoni <pietro.govoni@gmail.com>
2013-12-18 10:40:29 +0100

    count photons per chamfer

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -21,55 +21,42 @@
 #include "TString.h"
 
 
-EventAction::EventAction()
-{
-}
- 
-EventAction::~EventAction()
+EventAction::EventAction ()
 {
 }
 
-void EventAction::BeginOfEventAction(const G4Event* evt)
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+EventAction::~EventAction ()
 {
-  G4int evtNb = evt->GetEventID();
-  if( evtNb%1 == 0 ) 
-  {
-    G4cout << "---> Begin of Event: " << evtNb << G4endl;
-  }
-  
-  CreateTree::Instance() -> Clear();
-  
-  // --------------------- STORE INFO FOR X_0 / R_M ----------------------------- //
-  int Radial_nSteps       = 5000;
-  int Longitudinal_nSteps = 1000;
-  CreateTree::Instance() -> Radial_stepLength       = 1000. / Radial_nSteps;       // in mm
-  CreateTree::Instance() -> Longitudinal_stepLength = 5000. / Longitudinal_nSteps; // in mm
-  
-  // --------------------- STORE PRIMARY VERTEX ----------------------------- //
-  
-  if(CreateTree::Instance() -> Init_data())
-  {
-    G4double InitEnergy   = evt -> GetPrimaryVertex() -> GetPrimary() -> GetTotalEnergy();
-    G4ThreeVector InitPos = evt -> GetPrimaryVertex() -> GetPosition();
-    G4ThreeVector InitDir = evt -> GetPrimaryVertex() -> GetPrimary() -> GetMomentumDirection();
-    
-    CreateTree::Instance() -> InitialEnergy = InitEnergy/GeV;
-    
-    CreateTree::Instance() -> InitialPositionX = InitPos[0]/mm;
-    CreateTree::Instance() -> InitialPositionY = InitPos[1]/mm;
-    CreateTree::Instance() -> InitialPositionZ = InitPos[2]/mm;
-    
-    CreateTree::Instance() -> InitalMomentumDirectionX = InitDir[0];
-    CreateTree::Instance() -> InitalMomentumDirectionY = InitDir[1];
-    CreateTree::Instance() -> InitalMomentumDirectionZ = InitDir[2];
-  }
-    
-  // -------------------- INSTANCE RUN/EVENT IN TREE ---------------------- //
-  CreateTree::Instance() -> Event = evt -> GetEventID();
 }
 
-void EventAction::EndOfEventAction(const G4Event* evt)
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+void EventAction::BeginOfEventAction (const G4Event* evt)
+{
+  G4int evtNb = evt->GetEventID () ;
+  if ( evtNb%1 == 0 ) 
+  {
+    G4cout << "---> Begin of Event: " << evtNb << G4endl ;
+  }
+  
+  CreateTree::Instance ()->Clear () ;
+  
+  // INSTANCE RUN/EVENT IN TREE
+  CreateTree::Instance ()->Event = evt->GetEventID () ;
+}
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+void EventAction::EndOfEventAction (const G4Event* evt)
 { 
-  CreateTree::Instance()->Fill();
+  CreateTree::Instance ()->Fill () ;
 }
 
--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -30,30 +30,34 @@
 
 
 
-SteppingAction::SteppingAction()
+SteppingAction::SteppingAction ()
 {}
 
 
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-SteppingAction::~SteppingAction()
+
+SteppingAction::~SteppingAction ()
 {}
 
 
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-void SteppingAction::UserSteppingAction(const G4Step * theStep)
+
+void SteppingAction::UserSteppingAction (const G4Step * theStep)
 {
-  G4Track* theTrack = theStep->GetTrack();
-  G4ParticleDefinition* particleType = theTrack->GetDefinition();
+  G4Track* theTrack = theStep->GetTrack () ;
+  G4ParticleDefinition* particleType = theTrack->GetDefinition () ;
   
-  G4StepPoint* thePrePoint  = theStep->GetPreStepPoint();
-  G4StepPoint* thePostPoint = theStep->GetPostStepPoint();
-  G4VPhysicalVolume* thePrePV  = thePrePoint ->GetPhysicalVolume();
-  G4VPhysicalVolume* thePostPV = thePostPoint->GetPhysicalVolume();
-  G4String thePrePVName  = "" ; if( thePrePV )  thePrePVName  = thePrePV  -> GetName();
-  G4String thePostPVName = "" ; if( thePostPV ) thePostPVName = thePostPV -> GetName();
+  G4StepPoint* thePrePoint  = theStep->GetPreStepPoint () ;
+  G4StepPoint* thePostPoint = theStep->GetPostStepPoint () ;
+  G4VPhysicalVolume* thePrePV  = thePrePoint ->GetPhysicalVolume () ;
+  G4VPhysicalVolume* thePostPV = thePostPoint->GetPhysicalVolume () ;
+  G4String thePrePVName  = "" ; if ( thePrePV )  thePrePVName  = thePrePV  -> GetName () ;
+  G4String thePostPVName = "" ; if ( thePostPV ) thePostPVName = thePostPV -> GetName () ;
 
   // optical photon
-  if( particleType == G4OpticalPhoton::OpticalPhotonDefinition ())
+  if ( particleType == G4OpticalPhoton::OpticalPhotonDefinition ())
   { 
       // Check that the step starts and ends in any of the core volumes of any fibers.
       // This includes both the inner and outer unphysical sub-volumes of the core.
@@ -63,21 +67,24 @@ void SteppingAction::UserSteppingAction(const G4Step * theStep)
       pos   = thePostPVName.find ("FiberCore") ;
       if (pos == std::string::npos) return ;
 
+      int trackId = theTrack->GetTrackID () ;
+
       // Find the chamfer where the photon is traveling in.
       for (int i = 0 ; i < 4 ; ++i)
         {
-          string num_s = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
+          string num_s = static_cast<ostringstream*> ( & (ostringstream () << i) )->str () ;
           pos = thePrePVName.find (num_s) ;
           if (pos == std::string::npos) continue ;
           G4float length = theStep->GetStepLength () ;
 
-          // attribute the length to the chamfer
+          // give the length to the chamfer
           CreateTree::Instance ()->totalPhLengthInChamfer[i] += length/mm ;    
-//          cout << "    adding " << length 
-//               << " to " << CreateTree::Instance ()->totalPhLengthInChamfer[i]
-//               << " for chamfer " << i << endl ;
 
           // sum the lengths for each photon separately
+          CreateTree::Instance ()->addPhoton (trackId, length/mm, i) ;
+          
+          // once entered in a chamfer, does not loop on the following ones
+          break ;
         }
 
    } // optical photon
